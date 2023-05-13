20f2c2428d826e3f9868399381fce9a341d58409
Shreyan Wankavala <107050835+shrey1216@users.noreply.github.com>
2023-05-13 19:12:33 -0400

    Add files via upload

---
+++ EmptyStackException.java
@@ -0,0 +1,13 @@
+//@author Shreyan Wankavala
+//112634232
+//Recitation 01
+
+
+/** An Exception that's thrown when a stack is empty
+ *
+ */
+public class EmptyStackException extends Exception{
+    public EmptyStackException (String errorMessage) {
+        super(errorMessage);
+    }
+}
---
+++ FullStackException.java
@@ -0,0 +1,13 @@
+//@author Shreyan Wankavala
+//112634232
+//Recitation 01
+
+
+/** An Exception that's thrown when a stack is full
+ *
+ */
+public class FullStackException extends Exception{
+    public FullStackException (String errorMessage) {super(errorMessage);}
+}
+
+
